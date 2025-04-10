f16a9913f8812e71a6f292d681e5ebcd7c8e131b
pyankie <zpyankie@gmail.com>
2025-04-10 23:01:32 +0300

    feat(eventInvitation): Add invitation schema and DB insertion logic.

---
+++ src/models/eventInvitations.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+export interface IEventInvitation extends Document {
+  eventId: mongoose.Types.ObjectId;
+  email: string;
+  token: string;
+  status: "pending" | "accepted" | "expired";
+  createdAt: Date;
+  expiresAt: Date;
+}
+
+const eventInvitationSchema = new mongoose.Schema<IEventInvitation>(
+  {
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    email: { type: String, required: true },
+    token: { type: String, required: true, unique: true },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "expired"],
+      default: "pending",
+    },
+    createdAt: { type: Date, default: Date.now },
+    expiresAt: { type: Date, required: true },
+  },
+  { timestamps: true },
+);
+
+export const EventInvitation = mongoose.model<IEventInvitation>(
+  "EventInvitation",
+  eventInvitationSchema,
+);
--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -2,20 +2,17 @@ import mongoose from "mongoose";
 import { eventType } from "../models/event";
 import { Event, IEvent, IEventDocument } from "../models/event";
 import User from "../models/user";
+import { IEventInvitation, EventInvitation } from "../models/eventInvitations";
+import { v4 as uuidv4 } from "uuid";
+
+interface ISplitInvitees {
+  registeredIds: mongoose.Types.ObjectId[];
+  unregisteredEmails: string[];
+}
 
 export const createEvent = async (eventData: eventType) => {
   const { invitees, ...rest } = eventData;
-
-  const invitedEmails = invitees?.map((invt) => invt.email);
-  const registeredUsers = invitedEmails?.length
-    ? await User.find({ email: { $in: invitedEmails } }).select("_id email")
-    : [];
-
-  const registeredEmails = registeredUsers?.map((u) => u.email);
-  const registeredIds = registeredUsers?.map((u) => u._id);
-
-  const unregisteredEmails =
-    invitedEmails?.filter((email) => !registeredEmails.includes(email)) || [];
+  const { registeredIds, unregisteredEmails } = await splitInvtees(invitees);
 
   const newEvent = new Event({
     ...rest,
@@ -23,9 +20,47 @@ export const createEvent = async (eventData: eventType) => {
     organizerId: new mongoose.Types.ObjectId(rest.organizerId),
     invitees: registeredIds,
   });
-  return await newEvent.save();
+
+  const savedEvent = await newEvent.save();
+
+  if (unregisteredEmails.length > 0) {
+    const unregisteredInvitees = unregisteredEmails.map((email) => {
+      return {
+        eventId: newEvent._id,
+        token: uuidv4(),
+        email,
+        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      };
+    });
+
+    await EventInvitation.insertMany(unregisteredInvitees);
+  }
+  return savedEvent;
 };
 
+const splitInvtees = async (
+  invitees?: { email: string }[],
+): Promise<ISplitInvitees> => {
+  const invitedEmails = invitees?.map((invt) => invt.email);
+
+  const registeredUsers = invitedEmails?.length
+    ? await User.find({ email: { $in: invitedEmails } }).select("_id email ")
+    : [];
+
+  const registeredEmails = registeredUsers?.map((u) => u.email);
+
+  const registeredIds = registeredUsers?.map(
+    (u) => u._id,
+  ) as mongoose.Types.ObjectId[];
+
+  const unregisteredEmails =
+    invitedEmails?.filter((email) => !registeredEmails.includes(email)) || [];
+
+  return {
+    registeredIds,
+    unregisteredEmails,
+  };
+};
 const generateSlug = (
   title: string,
   uniqueIdentifier?: string | number,
