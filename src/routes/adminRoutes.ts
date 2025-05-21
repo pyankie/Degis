4adf8b3b5d3cfaabad539d9da3179824da30d0ea
pyankie <zpyankie@gmail.com>
2025-05-21 09:12:59 +0300

    feat(user): Add endpoint to change user status.

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import KycRequestController from "../controllers/kycRequestController";
 import { auth } from "../middlewares/auth";
 import { authorize } from "../middlewares/authRole";
+import { changeUserStatus } from "../controllers/adminController";
 const router = express.Router();
 
 router.get(
@@ -17,5 +18,7 @@ router.put(
   authorize(["admin"]),
   KycRequestController.verifyKycRequest,
 );
+
+router.put("/users/:id", authorize(["admin"]), changeUserStatus);
 
 export default router;
